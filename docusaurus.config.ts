a60e54a5ce82597a8bfb8335ef3125a9d3c43dec
mlengse <anjangkn@gmail.com>
2025-01-10 21:40:16 +0700

    docusaurus faster

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,48 +5,36 @@ import remarkHyphenopoly from './src/remark/hyphenopoly';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import 'dotenv/config'
-// import remarkHypher from 'remark-hypher'
-// import pattern from './hyphenation'
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
   title: 'ILP Puskesmas',
   tagline: 'Integrasi Pelayanan Kesehatan Primer',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
   url: 'https://ilp.jyg.my.id',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
   organizationName: 'mlengse', // Usually your GitHub org/user name.
   projectName: 'ilp-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'id',
     locales: ['id'],
   },
+  future: {
+    experimental_faster: true
+  },
   markdown: {
-    mermaid: true
+    mermaid: true,
   },
   themes: [
     '@docusaurus/theme-mermaid',
-    // [docusaurusSearchLocal, ({
-    //   hashed: true,
-    //   language: ['id']
-    // })]
   ],
   plugins: [
+    // './metaku',
     process.env.NODE_ENV === 'production' && '@docusaurus/plugin-debug',
   ].filter(Boolean),
   presets: [
@@ -58,55 +46,32 @@ const config: Config = {
           routeBasePath: '/',
           path: 'docs',
           sidebarPath: './sidebars.ts',
-          rehypePlugins: [rehypeKatex],
+          rehypePlugins: [
+            rehypeKatex,
+          ],
           remarkPlugins: [
             remarkMath,
-            // remarkHyphenopoly
             [remarkHyphenopoly, {
             //   // file: './patterns/id.wasm',
               lang: 'id',
               minWordLength: 5
             }]
-            // [remarkHypher, {
-            //   language: pattern,
-            //   leftmin: 2,
-            //   rightmin: 3,
-            //   minLength: 5,
-            // }]
           ],
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
           'https://github.com/mlengse/ilp-docs/blob/master/',
-          // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
           
         },
-        blog: false
-      //   blog: {
-      //     showReadingTime: true,
-      //     feedOptions: {
-      //       type: ['rss', 'atom'],
-      //       xslt: true,
-      //     },
-      //     // Please change this to your repo.
-      //     // Remove this to remove the "edit this page" links.
-      //     editUrl:
-      //       'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-      //     // Useful options to enforce blogging best practices
-      //     onInlineTags: 'warn',
-      //     onInlineAuthors: 'warn',
-      //     onUntruncatedBlogPosts: 'warn',
-      //   },
-      //   theme: {
-      //     customCss: './src/css/custom.css',
-      //   },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
       } satisfies Preset.Options,
     ],
   ],
 
   themeConfig: {
     
-    algolia: {
+    algolia: process.env.ALGOLIA_APP_ID ? {
       // The application ID provided by Algolia
       appId: process.env.ALGOLIA_APP_ID,
 
@@ -118,27 +83,10 @@ const config: Config = {
       // Optional: see doc section below
       contextualSearch: true,
 
-      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-      // externalUrlRegex: 'external\\.com|domain\\.com',
-
-      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-      // replaceSearchResultPathname: {
-      //   from: '/docs/', // or as RegExp: /\/docs\//
-      //   to: '/',
-      // },
-
-      // Optional: Algolia search parameters
-      // searchParameters: {
-      //   facetFilters: ['language:id'],
-      // },
-      // Optional: path for search page that enabled by default (`false` to disable it)
-      // searchPagePath: 'search',
-
-      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
       insights: false,
 
       //... other Algolia params
-    },
+    } : null ,
     // Replace with your project's social card
     image: 'img/download.png',
     navbar: {
@@ -154,57 +102,10 @@ const config: Config = {
           position: 'left',
           label: 'ILP',
         },
-        // {to: '/blog', label: 'Blog', position: 'left'},
-        // {
-        //   href: 'https://github.com/facebook/docusaurus',
-        //   label: 'GitHub',
-        //   position: 'right',
-        // },
       ],
     },
     footer: {
       style: 'dark',
-      // links: [
-      //   {
-      //     title: 'Docs',
-      //     items: [
-      //       {
-      //         label: 'Tutorial',
-      //         to: '/docs',
-      //       },
-      //     ],
-      //   },
-      //   {
-      //     title: 'Community',
-      //     items: [
-      //       {
-      //         label: 'Stack Overflow',
-      //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-      //       },
-      //       {
-      //         label: 'Discord',
-      //         href: 'https://discordapp.com/invite/docusaurus',
-      //       },
-      //       {
-      //         label: 'X',
-      //         href: 'https://x.com/docusaurus',
-      //       },
-      //     ],
-      //   },
-      //   {
-      //     title: 'More',
-      //     items: [
-      //       {
-      //         label: 'Blog',
-      //         to: '/blog',
-      //       },
-      //       {
-      //         label: 'GitHub',
-      //         href: 'https://github.com/facebook/docusaurus',
-      //       },
-      //     ],
-      //   },
-      // ],
       copyright: `© ${new Date().getFullYear()} Puskesmas Jayengan`,
     },
     prism: {
